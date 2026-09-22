114dd3d26d44934d30f0adf41fd641d993b932b5
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2026-09-22 07:27:27 +0100

    complete toot-tip working project

---
+++ tool-tip/dist/tool-tip.672d4772.js
@@ -0,0 +1,6 @@
+const tooltip = document.querySelector("[data-tooltip]");
+document.getElementsByClassName("container").addEventListener("mouseover", (e)=>{
+    e.target.classList.add("tooltip");
+});
+
+//# sourceMappingURL=tool-tip.672d4772.js.map
