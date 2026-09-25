cc858274ba4bdac6d3c0728e9941543661f361a1
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2026-09-25 08:21:54 +0100

    implementing drag features

--- trello-clone/dragAndDrop.js
+++ trello-clone/dragAndDrop.js
@@ -2,6 +2,50 @@ import addGlobalEventListener from "./utils/addGlobalEventListener";
 
 export default function setup() {
   addGlobalEventListener("mousedown", "[data-draggable]", (e) => {
-    console.log("mousedown");
+    const selectedItem = e.target;
+    const itemClone = selectedItem.cloneNode(true);
+    const offset = setupDragItems(selectedItem, itemClone, e);
+
+    setupDragEvent(selectedItem, itemClone, offset);
   });
+}
+
+function setupDragItems(selectedItem, itemClone, e) {
+  const originalRect = selectedItem.getBoundingClientRect();
+  const offset = {
+    x: e.clientX - originalRect.left,
+    y: e.clientY - originalRect.top,
+  };
+
+  selectedItem.classList.add("hide");
+
+  itemClone.style.width = `${originalRect.width}px`;
+  itemClone.classList.add("dragging");
+  positionClone(itemClone, e, offset);
+  document.body.append(itemClone);
+
+  return offset;
+}
+
+function setupDragEvent(selectedItem, itemClone, offset) {
+  const mouseMoveFunction = (e) => {
+    positionClone(itemClone, e, offset);
+  };
+
+  document.addEventListener("mousemove", mouseMoveFunction);
+  document.addEventListener(
+    "mouseup",
+    () => {
+      document.removeEventListener("mousemove", mouseMoveFunction);
+      selectedItem.classList.remove("hide");
+      itemClone.remove();
+      console.log("up");
+    },
+    { once: true },
+  );
+}
+
+function positionClone(itemClone, mousePosition, offset) {
+  itemClone.style.top = `${mousePosition.clientY - offset.y}px`;
+  itemClone.style.left = `${mousePosition.clientX - offset.x}px`;
 }
